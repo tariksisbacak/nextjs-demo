fc83fff5bf410664f5f6c067366f66361511947f
Tarık Şişbacak <tariksisbacak@gmail.com>
2022-06-08 18:14:16 +0300

    Ana Sayfa giriş yazısı değişti

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          İlk web siteme hoşgeldin <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
